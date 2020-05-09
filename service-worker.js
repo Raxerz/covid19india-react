d65f01ee405bc1c01545e512c3cac070f39e155c
Raxerz <rajath_krishna@yahoo.co.in>
2020-05-09 17:59:55 +0000

    Deploying to gh-pages from  @ e963433f12c6f39af573db3a382400c746a7046d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe7fba27cc011b0f05b5d0f10b2eb6cb.js"
+  "/precache-manifest.a0325ee19daff44b6de6a7f7257295b7.js"
 );
 
 self.addEventListener('message', (event) => {
